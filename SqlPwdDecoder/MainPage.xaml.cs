74e4f40953420a51c246881cda451a8cb38a3565
Angel Gustavo Garza Aguilar <angel_winy@live.com.mx>
2021-05-31 16:11:22 -0500

    Add project files.

---
+++ SqlPwdDecoder/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SqlPwdDecoder
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCode_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                txtResult.Text = string.Empty;
+                txtResult.Text = new PwdCodder().CodePassword(txtpwd.Text);
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = ex.Message + "\n\n" + ex.InnerException?.Message;
+            }
+        }
+
+        private void btnDecode_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                txtpwd.Text = string.Empty;
+                txtpwd.Text = new PwdCodder().DecodePassword(txtResult.Text);
+            }
+            catch (Exception ex)
+            {
+                txtResult.Text = ex.Message + "\n\n" + ex.InnerException?.Message;
+            }
+        }
+    }
+}
---
+++ SqlPwdDecoder/PwdCodder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SqlPwdDecoder
+{
+    public class PwdCodder
+    {
+        public PwdCodder()
+        {
+
+        }
+
+        public string CodePassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+                return string.Empty;
+
+            string result = "";
+            var bytes = Encoding.ASCII.GetBytes(password.Reverse().ToArray()).Select(x => x.ToString());
+
+            foreach (var digit in bytes)
+            {
+                if (digit.Length == 2)
+                {
+                    result += "4";
+                    result += digit;
+                }
+                else
+                {
+                    result += "6";
+                    result += digit;
+                }
+            }
+            return result;
+        }
+
+        public string DecodePassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+                return string.Empty;
+
+            List<byte> partes = new List<byte>();
+            while (!string.IsNullOrWhiteSpace(password))
+            {
+                if (password.First() == '4')
+                {
+                    partes.Add(byte.Parse(password.Substring(1, 2)));
+                    password = password.Remove(0, 3);
+                }
+                else if (password.First() == '6')
+                {
+                    partes.Add(byte.Parse(password.Substring(1, 3)));
+                    password = password.Remove(0, 4);
+                }
+                else
+                {
+                    throw new ArgumentException("password is not in the correct format");
+                }
+            }
+
+            string result = Encoding.ASCII.GetString(partes.ToArray());
+            return new string(result.Reverse().ToArray());
+        }
+    }
+}
